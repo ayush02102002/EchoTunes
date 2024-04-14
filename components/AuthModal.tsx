a80ec61080932d7e017b75750b519974081ea5c1
ayush02102002 <92004465+ayush02102002@users.noreply.github.com>
2024-04-14 23:40:20 +0530

    Update AuthModal.tsx

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -37,7 +37,7 @@ const AuthModal = () => {
         <Auth
             theme="dark"
             magicLink
-            providers={}
+            provider={}
             supabaseClient={supabaseClient}
             appearance={{
                 theme:ThemeSupa,
